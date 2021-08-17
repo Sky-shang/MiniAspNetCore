634f49bb700fe829a76ff13da1e70caa5e8932cd
Sky-Shang <1006979524@qq.com>
2021-08-17 23:10:22 +0800

    Initial MiniAspNetCore

---
+++ MiniAspNetCore/FeatureCollection.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MiniAspNetCore
+{
+    public class FeatureCollection : Dictionary<Type, object>, IFeatureCollection { }
+}
---
+++ MiniAspNetCore/IFeatureCollection.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniAspNetCore
+{
+    public interface IFeatureCollection : IDictionary<Type, object> { }
+}
